c06849278cfc4939285811b9924a57c88cc27434
James Hennessy <james.ryan.hennessy@gmail.com>
2017-05-01 09:35:34 -0700

    Add files via upload

---
+++ make (1).php
@@ -0,0 +1,84 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "HiMommy12";
+$dbname = "GNPNDB";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+     die("Connection failed: " . $conn->connect_error);
+          }
+
+$table="testy";
+$sql="SELECT * FROM testy ";
+$full=array();
+$result = $conn->query($sql);
+
+if ($result->num_rows >0) {
+ while($row = $result->fetch_assoc()) {
+          $full[]=$row;
+
+
+}
+}
+
+for($n=4; $n>1;$n--){
+$sql="SELECT * FROM testy WHERE CDS LIKE '%".$n."'";
+
+$ons=array();
+$cds=array();
+$result = $conn->query($sql);
+if ($result->num_rows >0) {
+   		      
+    // output data of each row
+            while($row = $result->fetch_assoc()) {
+	    	       $id= $row['GeneID'];
+		       $place=array_column($full,'GeneID');
+		       $key=array_search($id,$place);
+		       $length= strlen($full[$key]['ONS']);
+		       $keys=$key-1;
+		       echo $keys;
+		       echo $key;
+		       $full[$keys]['ONS']= $full[$keys]['ONS'].substr($full[$key]['ONS'],50,$length);
+			 
+		       
+		       
+		       
+		      
+		      
+}
+
+}
+
+}
+$fp=fopen("new.csv","w");
+
+foreach ($full as $fields) {
+    fputcsv($fp, $fields);
+    }
+fclose($fp);
+$place=array_column($full,'GeneID');
+$length= count($place);
+for($key=0;$key<$length;$key++){
+$sql = "UPDATE Gene SET ONS ='".$full[$key]['ONS']."' WHERE GeneID=".$place[$key];
+echo $key."<br>";
+$conn->query($sql);
+}
+for($l=4;$l>1;$l--){
+
+$sql="DELETE FROM Gene WHERE CDS LIKE '%".$l."'";
+echo "Best";
+$conn->query($sql);
+}
+//for($cds in $value){
+
+
+
+//}
+    
+
+?>
+
+<button type="button" onclick="location.href='dload.php'">here</button>
---
+++ rake (1).php
@@ -0,0 +1,93 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "HiMommy12";
+$dbname = "GNPNDB";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+     die("Connection failed: " . $conn->connect_error);
+               }
+$table="Beiber";
+$sql="SELECT * FROM `mama`";
+$result= $conn->query($sql);
+$full=array();
+echo "Born";
+$i=0;
+if ($result->num_rows >0) {
+   echo "Yolo"."<br>";
+ while($row = $result->fetch_assoc()) {
+ 	  $full[]=$row;
+	 
+	  print_r($row);
+	  echo $row['POS']."<br>";
+          if($row['POS']=='1f'){
+		echo "Hello"."<br>";
+		$full[$i]['Promoter']=60;
+		$full[$i]['Terminator']=7;
+	  }
+	  else if($row['POS']=='1'){
+			echo "Hellod"."<br>";
+			
+                $full[$i]['Promoter']=26;
+		$full[$i]['Terminator']=15;
+          }
+          else if($row['POS']=='2'){
+			echo "Hellos"."<br>";
+			
+                $full[$i]['Promoter']=55;
+                $full[$i]['Terminator']=5;
+          }
+          else if($row['POS']=='3'){
+			echo "Hfello"."<br>";
+			
+                $full[$i]['Promoter']=17;
+                $full[$i]['Terminator']=3;
+          }
+	   else if($row['POS']=='4'){
+			echo "Kello"."<br>";
+			
+                $full[$i]['Promoter']=26;
+                $full[$i]['Terminator']=15;
+          }
+          else if($row['POS']=='5'){
+			echo "Jello"."<br>";
+			
+                $full[$i]['Promoter']=59;
+                $full[$i]['Terminator']=1;
+          }
+          else if($row['POS']=='6'){
+			echo "Trdello"."<br>";
+			
+                $full[$i]['Promoter']=57;
+                $full[$i]['Terminator']=2;
+          }
+          else if($row['POS']=='7'){
+			echo "bello"."<br>";
+			
+                $full[$i]['Promoter']=26;
+                $full[$i]['Terminator']=7;
+          }
+
+
+	  $i=$i+1;
+	}
+}
+$length=count(array_column($full,'Promoter'));
+for($i=1;$i<$length;$i++){
+	if( $full[$i]['POS']<$full[$i-1]['POS']){
+	    $full[$i-1]['Terminator']='7';  
+
+	}
+
+}
+$fp=fopen("news.csv","w");
+
+foreach ($full as $fields) {
+    fputcsv($fp, $fields);
+    }
+fclose($fp);
+?>
+<button type="button" onclick="location.href='dloads.php'">here</button>
